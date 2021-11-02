4f1a508d536cd29c5ec1296336f42ac1c8635da7
mrSelector <pasha-o@i.ua>
2021-11-02 22:08:49 +0200

    lesson5-6

--- orm/urls.py
+++ orm/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, re_path
-from .views import IndexTemplate
+from .views import IndexTemplate, delete_record
 urlpatterns = [
-    path('', IndexTemplate.as_view())
+    path('', IndexTemplate.as_view()),
+    re_path(r'^([\d]{1})$', delete_record)
 ]
--- orm/views.py
+++ orm/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views import View
-from django.views.generic import TemplateView
 from . models import Human
 
 
@@ -15,4 +15,10 @@ class IndexTemplate(View):
         return render(request, self.template_name, context)
 
 
-
+def delete_record(request, id):
+    print(id)
+    obj_list = Human.objects.filter(id=id)
+    if not obj_list:
+        return HttpResponse('Удаляемые данные не найдены')
+    obj_list.delete()
+    return redirect('/')
